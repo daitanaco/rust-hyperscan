4b9a8800d60ba6e2447e88eb4ecc466b7e7b501b
Jonathan <eisen@discourse.ai>
2022-04-11 02:56:09 -0500

    Added multiple regexes

--- hyperscan/examples/simplegreps.rs
+++ hyperscan/examples/simplegreps.rs
@@ -48,8 +48,16 @@ fn main() -> Result<()> {
     // match newline characters. The compiler will analyse the given pattern and
     // either return a compiled Hyperscan database, or an error message
     // explaining why the pattern didn't compile.
-    //
-    let patterns = patterns!("test", "foo", "bar"; CASELESS | DOTALL | SOM_LEFTMOST);
+    let pattern1 = r#"(?<us_currency>\$[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2}))"#;
+    let pattern2 = r#"(?<us_social_security_number>[0-8][0-9]{2}(-|\s)?[0-9]{2}(-|\s)[0-9]{4})"#;
+    let pattern3 = r#"(?<credit_card>\b4\d{3}[\s-]?\d{4}[\s-]?\d{4}[\s-]?\d{4}\b)"#;
+    let pattern4 = r#"(?<credit_card>\b5[1-5]\d{2}[\s-]?\d{4}[\s-]?\d{4}[\s-]?\d{4}\b)"#;
+    let pattern5 = r#"(?<credit_card>\b6011[\s-]?\d{4}[\s-]?\d{4}[\s-]?\d{4}\b)"#;
+    let pattern6 = r#"(?<credit_card>\b3[4,7]\d{2}[\s-]?\d{6}[\s-]?\d{5}\b)"#;
+    let pattern7 = r#"(?:[a-z0-9!#$%&''*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#;
+    let pattern8 = r#"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"#;
+    let pattern9 = r#"\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})"#;
+    let patterns = patterns!(pattern1, pattern2, pattern3, pattern4, pattern5, pattern6, pattern7, pattern8, pattern9; CASELESS | DOTALL | SOM_LEFTMOST);
     let db: BlockDatabase = patterns.build().unwrap();
 
     // Next, we read the input data file into a buffer.
@@ -76,10 +84,10 @@ fn main() -> Result<()> {
     
     println!("Scanning {} bytes with Hyperscan", input_data.len());
     db
-        .scan(&input_data, &scratch, |_, from, to, _| {
+        .scan(&input_data, &scratch, |id, from, to, flags| {
             println!(
                 "Match for pattern \"{}\" at offset {}..{}: {}",
-                patterns[0].expression,
+                id,
                 from,
                 to,
                 &input_data[from as usize..to as usize]
